16174f606aaef3b07570cb28b0a82cab90a3ee1a
giorgi-kul <kulijanishvilig@gmail.com>
2022-11-02 18:04:42 +0400

    extension tests

---
+++ src/Tests/ElectricityDataApp.UnitTests/ExtensionTests.cs
@@ -0,0 +1,31 @@
+﻿using ElectricityDataApp.DataParser.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectricityDataApp.UnitTests
+{
+    public class ExtensionTests
+    {
+        [Theory]
+        [InlineData("2022.11", true)]
+        [InlineData("2022-11", false)]
+        [InlineData("2022/11", false)]
+        [InlineData("11.2022", false)]
+        public void ToNormalizedDateTime_Should_Return_DateTime(string date, bool shouldConvertSuccessfully)
+        {
+            try
+            {
+                date.ToNormalizedDate();
+
+                Assert.True(shouldConvertSuccessfully);
+            }
+            catch
+            {
+                Assert.False(shouldConvertSuccessfully);
+            }
+        }
+    }
+}
